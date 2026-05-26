db51a9026f27f77e8989d70b61334c61d1a67d98
muslimbekuz <muslimbekkhalimjonov@gmail.com>
2026-05-26 16:19:50 +0900

    feat: integration of envirinmwntal variables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,2 @@
-console.log("EXECUTED");
-
-import moment from "moment";
-
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
-
-const person: string = "Tony";
-const count: number = 23;
+import dotenv from "dotenv";
+dotenv.config();
