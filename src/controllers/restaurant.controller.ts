186f008920d6cafaba581a9eeb9f4bfbe227251b
muslimbekuz <muslimbekkhalimjonov@gmail.com>
2026-06-10 20:00:16 +0900

    feat: locals variable integration

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
@@ -110,6 +110,22 @@ restaurantController.checkAuthSession = async (
     else res.send(` <script> alert("${Message.NOT_AUTHENTICATED}")</script>`);
   } catch (err) {
     console.log("Error, checkAuthSession:", err);
+  }
+};
+
+restaurantController.verifyRestaurant = (
+  req: AdminRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (req.session?.member?.memberType === MemberType.RESTAURANT) {
+    req.member = req.session.member;
+    next();
+  } else {
+    const message = Message.NOT_AUTHENTICATED;
+    res.send(
+      ` <script> alert("${message}"); window.location.replace('/admin/login')</script>`,
+    );
   }
 };
 
