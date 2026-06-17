6d72083ea83453a9dbb43b3d8486d1abcf418047
muslimbekuz <muslimbekkhalimjonov@gmail.com>
2026-06-17 19:07:12 +0900

    feat: develop updateChosenUser api

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -42,4 +42,9 @@ routerAdmin.get(
   restaurantController.verifyRestaurant,
   restaurantController.getUsers,
 );
+routerAdmin.post(
+  "/user/edit",
+  restaurantController.verifyRestaurant,
+  restaurantController.updateChosenUser,
+);
 export default routerAdmin;
