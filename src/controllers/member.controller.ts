108878a1fe35484ab3a3dc5fc7bedc90d7e9d743
muslimbekuz <muslimbekkhalimjonov@gmail.com>
2026-07-07 02:36:44 +0900

    feat: create getRestaurant rest Api

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -17,6 +17,19 @@ const authService = new AuthService();
 
 const memberController: T = {};
 
+memberController.getRestaurant = async (req: Request, res: Response) => {
+  try {
+    console.log("getRestaurant");
+    const result = await memberService.getRestaurant();
+
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error, processSignUp:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
 memberController.SignUp = async (req: Request, res: Response) => {
   try {
     console.log("SignUp");
--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,7 @@ import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader";
 
 /* Member */
+router.get("/member/restaurant", memberController.getRestaurant);
 router.post("/member/login", memberController.Login);
 router.post("/member/signup", memberController.SignUp);
 router.post(
